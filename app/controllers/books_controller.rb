4334e088976bb807cf7ac81ab9214dbe68bf1967
chororinko <147678517+chororinko@users.noreply.github.com>
2023-11-16 14:10:31 +0000

    [Now] 4-27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  
+
   def new
     @book = Book.new
   end
@@ -7,17 +7,33 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path
-  end 
+    if @book.save
+      redirect_to book_path(current_user.id)
+    else
+      render :index
+    end
+  end
 
   def index
     @books = Book.all
+    # 以下投稿したユーザーのトプ画載せたい
+
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
+
+
+
+

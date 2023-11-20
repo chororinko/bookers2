0579df2d3e3407375613eed4eab4ce72dc4fcb43
chororinko <147678517+chororinko@users.noreply.github.com>
2023-11-20 14:31:25 +0000

    [Now] 4-27

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,8 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  validates :name, presence: true, length: { minimum: 2 }
-  validates :email, presence: true
-  validates :password, presence: true
+  # validates :name, presence: true, length: { minimum: 2 }
+  # validates :email, presence: true
+  # validates :password, presence: true
 
 end

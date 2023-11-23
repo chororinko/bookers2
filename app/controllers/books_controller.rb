a343f1c011e63f88ea9172e90939a4dcb7f5ff90
chororinko <147678517+chororinko@users.noreply.github.com>
2023-11-23 10:51:23 +0000

    [Finish] 4-27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,6 @@
 class BooksController < ApplicationController
 before_action :is_matching_login_user, only: [:edit, :update]
 
-  def new
-    @book = Book.new
-  end
-
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id

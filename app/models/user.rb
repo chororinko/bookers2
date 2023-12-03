196b2a1bf9638e3a8a8acf52e11e3183ce6cf203
chororinko <147678517+chororinko@users.noreply.github.com>
2023-12-03 13:32:24 +0000

    [Now] phase2-4

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
-  has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :followings, through: :followers, source: :followed
-  has_many :followers, through: :followeds, source: :follower
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :relationships, source: :follower
 
 
   validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
@@ -28,11 +28,11 @@ class User < ApplicationRecord
   end
 
   def follow(user_id)
-    followers.create(followed_id: user_id)
+    relationships.create(followed_id: user_id)
   end
 
   def unfollow(user_id)
-    followers.find_by(followed_id: user_id).destroy
+    relationships.find_by(followed_id: user_id).destroy
   end
 
   def following?(user)

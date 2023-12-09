4e978e98ac0cbbc865f62ee1ba69cde2c82d76eb
chororinko <147678517+chororinko@users.noreply.github.com>
2023-12-09 02:39:35 +0000

    [Now] phase2-7a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,13 @@ before_action :is_matching_login_user, only: [:edit, :update]
   end
 
   def index
-    @books = Book.all
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorited_users).
+      sort_by {|x|
+        x.favorited_users.includes(:favorites).where(created_at: from...to).size
+      }.reverse
+    # @books = Book.all
     @book = Book.new
     @user = current_user
   end

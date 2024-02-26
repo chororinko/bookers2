5df687ecb6c587bf4ea80711dcba1169c1f2969c
chororinko <147678517+chororinko@users.noreply.github.com>
2024-02-26 05:56:34 +0000

    modified: add comment for notification

--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -1,6 +1,11 @@
 class BookComment < ApplicationRecord
   belongs_to :user
   belongs_to :book
+  has_one :notification, as: :notifiable, dependent: :destroy
+
+  after_create do
+    create_notification(user_id: book.user_id)
+  end
 
   validates :comment, presence: true
 
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -7,16 +7,20 @@ class Notification < ApplicationRecord
   def message
     if notifiable_type === "Book"
       "フォローしている#{notifiable.user.name}さんが#{notifiable.title}を投稿しました"
-    else
+    elsif notifiable_type === "Favorite"
       "投稿した#{notifiable.book.title}が#{notifiable.user.name}さんにいいねされました"
+    else
+      "投稿した#{notifiable.book.title}に#{notifiable.user.name}さんがコメントしました"
     end
   end
 
   def notifiable_path
     if self.notifiable_type === "Book"
       book_path(self.notifiable.id)    # 投稿に対する通知の場合はBookの詳細ページへ
-    else
+    elsif notifiable_type === "Favorite"
       user_path(self.notifiable.user.id)     # いいねに対する通知の場合はいいねをしたUserの詳細ページへ
+    else
+      book_path(self.notifiable.book.id)    # コメントに対する通知の場合はコメントをしたBookの詳細ページへ
     end
   end
 
